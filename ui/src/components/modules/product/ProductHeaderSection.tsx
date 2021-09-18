21308ab2413fb9a8eeeb793d53e6f38a2ab23d74
thanhlongb <s3748575@rmit.edu.vn>
2021-09-18 22:40:19 +0700

    changed 'Waiting confirm' to 'Waiting confirmation'

--- ui/src/components/modules/product/ProductHeaderSection.tsx
+++ ui/src/components/modules/product/ProductHeaderSection.tsx
@@ -163,7 +163,7 @@ const ProductHeaderSection = ({
               </Link>              
              ) : (
                 <div className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-yellow-500  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500">
-                Waiting confirm
+                Waiting confirmation
                 </div>
              ) 
           ) : (
